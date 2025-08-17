d192f9fd552f20e3329a7e555cf76dc9f9a5455e
Gordon <GordonAccount@yahoo.com>
2025-08-17 01:48:27 -0400

    Clean up navbar implementation

--- js/loaderComponents.js
+++ js/loaderComponents.js
@@ -11,6 +11,17 @@
         document.getElementById(targetId).innerHTML = html;
       })
     );
+
+    // Highlight active navbar link
+    const currentPage = window.location.pathname.split("/").pop() || "index.html";
+    const navLinks = document.querySelectorAll("#include-navbar .nav-link");
+    navLinks.forEach(link => {
+        const linkPage = link.getAttribute("href").split("/").pop();
+        if (linkPage === currentPage) {
+            link.classList.remove("richLink");
+            link.classList.add("richLinkHighlight");
+        }
+    });
   
     console.log("Fragments loaded, dispatching event...");
     document.dispatchEvent(new Event("fragmentsLoaded"));

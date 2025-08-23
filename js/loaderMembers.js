ebb9c724437d2c339bfb02d356dd7879584dbb86
Gordon <GordonAccount@yahoo.com>
2025-08-23 03:00:48 -0400

    Update team page

---
+++ js/loaderMembers.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('./assets/data/members.json')
+        .then(response => response.json())
+        .then(members => {
+            const container = document.getElementById('team-container');
+            members.forEach(member => {
+                const memberDiv = document.createElement('div');
+                memberDiv.className = 'col-xl-4 col-lg-4';
+                memberDiv.innerHTML = `
+                    <div class="exec-padding">
+                        <div class="exec-container">
+                            <img class="img-fluid exec-image" src="assets/members/${member.image}" />
+                            <div class="member-info">
+                                <div class="member-name">${member.name}</div>
+                                <div class="member-role">${member.position}</div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                container.appendChild(memberDiv);
+            });
+        })
+        .catch(error => console.error('Error loading team members:', error));
+});

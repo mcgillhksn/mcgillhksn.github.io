4a6855de6a9b02cb8c0724a5ae6f2be8c0c18c0a
Gordon <GordonAccount@yahoo.com>
2025-08-24 01:02:19 -0400

    Add images for team page

--- js/loaderMembers.js
+++ js/loaderMembers.js
@@ -1,3 +1,7 @@
+// 
+// 
+// 
+// 
 document.addEventListener("DOMContentLoaded", function() {
     fetch('./assets/data/members.json')
         .then(response => response.json())
@@ -5,11 +9,11 @@ document.addEventListener("DOMContentLoaded", function() {
             const container = document.getElementById('team-container');
             members.forEach(member => {
                 const memberDiv = document.createElement('div');
-                memberDiv.className = 'col-xl-4 col-lg-4';
+                memberDiv.className = 'col-lg-4 col-md-6 col-sm-12';
                 memberDiv.innerHTML = `
                     <div class="exec-padding">
                         <div class="exec-container">
-                            <img class="img-fluid exec-image" src="assets/members/${member.image}" />
+                            <img class="img-fluid exec-image" src="assets/members/2024-2025/${member.image}" />
                             <div class="member-info">
                                 <div class="member-name">${member.name}</div>
                                 <div class="member-role">${member.position}</div>
